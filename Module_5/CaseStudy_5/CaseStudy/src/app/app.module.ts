1afba6d5f86372b2d4e973e83f304247a84c1ef7
C0920G1-Tran-Minh-Chien <chiennext@gmail.com>
2021-02-26 08:41:25 +0700

    up

--- Module_5/CaseStudy_5/CaseStudy/src/app/app.module.ts
+++ Module_5/CaseStudy_5/CaseStudy/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
-import { ModelComponent } from './model/model.component';
+import { CreateCustomerComponent } from './components/customer/create-customer/create-customer.component';
+import { UpdateCustomerComponent } from './components/customer/update-customer/update-customer.component';
+import { ListCustomerComponent } from './components/customer/list-customer/list-customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
-    ModelComponent
+    CreateCustomerComponent,
+    UpdateCustomerComponent,
+    ListCustomerComponent
   ],
   imports: [
     BrowserModule,
